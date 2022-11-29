eb6c94a147950ddc9b67bddac9fda583b87af886
goodwinjose26 <josegoodwin22@gmail.com>
2022-11-29 08:24:45 +0530

    view news

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ViewnewsComponent } from './viewnews/viewnews.component';
+const myRoute:Routes=[
+  {
+    path:"",
+    component:ViewnewsComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewnewsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
